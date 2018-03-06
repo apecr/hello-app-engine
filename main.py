088d984130ca1e6c9220eab2dd3af4b84750546d
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-06 15:18:47 +0100

    Include the greetings database and html in the requesthandler

--- main.py
+++ main.py
@@ -14,17 +14,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.ext import ndb
 import webapp2
+
+
+class Greetings(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    message = ndb.TextProperty(required=True)
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        html = """
+        <html>
+        
+        <head>
+        </head>
+        
+        <body>
+        <h1>My Guest Book</h1>
+        
+        <form action="/greet" method="post">
+            <p>
+                Name: <input type="text" name="user_name" style="width: 300px">
+            </p>
+            <p>
+                Message: <textarea name="message" style="width: 300px" rows=5></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Send" >
+            </p>
+        </form>
+        
+        <h1>Past Greetings</h1> 
+        """
+        greetings = Greetings.query().order(-Greetings.timestamp).fetch()
+
+        for greet in greetings:
+            html += greet.timestamp.strftime('%Y-%m-%d %H:%m') + " - " + greet.name + " said " + greet.message + "<br>"
+
+        html += """
+        </body>
+        </html>
+        """
+        self.response.write(html)
+
+
+class GreetHandler(webapp2.RequestHandler):
+    def post(self):
+        user_name = self.request.get('user_name')
+        message = self.request.get('message')
+        Greetings(name=user_name, message=message).put()
+        self.redirect('/')
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/greet', GreetHandler)
 ], debug=True)
+
 
 
 
